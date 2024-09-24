864e1ddb5783a18bf8ede283dce384a97c43a37c
Luke <69825619+LukePerezz@users.noreply.github.com>
2024-09-24 15:36:29 -0700

    CSE360 First Commit of ASUHelloWorld

---
+++ ASUHelloWorldJavaFXMavenHW3/src/main/java/edu/asu/ASUHelloWorldJavaFXMaven/HelloFX.java
@@ -0,0 +1,36 @@
+package edu.asu.ASUHelloWorldJavaFXMaven;
+
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+
+public class HelloFX extends Application {
+
+    public void start(Stage primaryStage) {
+    	System.out.println("Luke Perez Hello World!");
+    	System.out.println("It started!");
+        primaryStage.setTitle("Luke Perez Hello World Fall 2024");
+        Button btn = new Button();
+        btn.setText("Display: 'Luke Perez says: Hello World!'");
+        btn.setOnAction(new EventHandler<>() {
+            public void handle(ActionEvent event) {
+                System.out.println("Luke Perez says: Hello World!");
+            }
+        });
+        
+        StackPane root = new StackPane();
+        root.getChildren().add(btn);
+        primaryStage.setScene(new Scene(root, 400, 250));
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+}
